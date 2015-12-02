4a80aad45060f7a3bd1b4cb22e69e5cca843f543
Tom Barnes <ruralmyth@gmail.com>
2015-12-02 18:04:44 +0000

    fix package.json for heroku

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var path = require('path');
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (req, res) {
+  res.render('./index');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+});
