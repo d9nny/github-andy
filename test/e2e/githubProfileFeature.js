9885e616b9037f32331a124f7b69f4f8e2b84cf2
Tom Barnes <ruralmyth@gmail.com>
2015-12-02 15:42:37 +0000

    added protractor dependency and first feature test

---
+++ test/e2e/githubProfileFeature.js
@@ -0,0 +1,6 @@
+describe('Githib Profile finder', function() {
+  it ('has a title', function() {
+    browser.get('http://localhost:8080');
+    expect(browser.getTitle()).toEqual('Github user search');
+  });
+});
