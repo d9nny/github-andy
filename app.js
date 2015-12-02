a95e67381f7025830a60c6102fa474613559dd17
Tom Barnes <ruralmyth@gmail.com>
2015-12-02 18:41:32 +0000

    heroku fix

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var app = express();
 
-var path = require('path');
 app.use(express.static(__dirname + '/'));
 app.set('views', __dirname + '/');
 
 app.get('/', function (req, res) {
   res.render('./index');
 });
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 
 // var server = app.listen(3000, function () {
 //   var host = server.address().address;
