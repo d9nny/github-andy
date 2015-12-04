1dbabdd43f782d96639423649807a1f3368e8121
Andrew Dowell <adowellphoto@gmail.com>
2015-12-04 12:08:58 +0000

    amendmentsg

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -6,7 +6,7 @@ githubUserSearch.controller('GitUserSearchController', ['Search', function(Searc
     Search.query(self.searchTerm)
       .then(function(response) {
         self.searchResult = response.data;
-        console.log(self.searchResult);
+        console.log(response.data);
       })
     };
 
@@ -18,16 +18,18 @@ githubUserSearch.controller('GitUserSearchController', ['Search', function(Searc
     return self.currentReveal === userObj;
   }
 
-  self.picked = function(url) {
-    self.this = url;
-    console.log(url);
+  self.apiCall = function(url) {
     Search.reveal(url)
     .then(function(response) {
       angular.extend(self.currentReveal, response.data);
     })
   }
 
-  self.revealOne = function(url) {
-    return self.this === url;
-  }
+  self.setTab = function(tab) {
+    self.activeTab = tab;
+  };
+
+  self.isActive = function(tab) {
+    return (self.activeTab === tab);
+  };
 }]);
