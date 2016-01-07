f6fd20cb901fbbd111b3ee812e019791e61c22ec
danwheeler <da-wheeler@hotmail.co.uk>
2016-01-07 18:58:06 +0000

    updated UI

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,29 +1,33 @@
 githubUserSearch.controller('GitUserSearchController', ['Search', function(Search) {
 
   var self = this;
+  self.currentReveal = {};
+
+
 
   self.doSearch = function () {
     Search.query(self.searchTerm)
       .then(function(response) {
         self.searchResult = response.data;
-        console.log(response.data);
       })
     };
 
-  self.reveal = function(userObj) {
-    self.currentReveal = userObj
-    };
+  self.setSelected = function(tab) {
+    self.activeSelection = tab;
+    self.activeTab = 1;
+  };
 
-  self.isRevealed = function(userObj) {
-    return self.currentReveal === userObj;
-  }
+  self.selected = function() {
+    return self.activeSelection === 1;
+  };
 
   self.apiCall = function(url) {
+        console.log(url);
     Search.reveal(url)
     .then(function(response) {
       angular.extend(self.currentReveal, response.data);
     })
-  }
+  };
 
   self.setTab = function(tab) {
     self.activeTab = tab;
